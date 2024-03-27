36d76688d3f17980a4dd3b98071bc9f97076a069
jbiddulph <john.mbiddulph@gmail.com>
2024-03-27 23:30:02 +0000

    sorted images in listings

--- coasterz/urls.py
+++ coasterz/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 from . import views
@@ -13,3 +15,7 @@ urlpatterns = [
   path('api/notes/', include('notes.urls')),
   path('api/events/', include('events.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
